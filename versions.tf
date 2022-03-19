9819eea2a6ff589e4f749c58fb58800bdcb5a4fe
huan11 <huan.meng@ibm.com>
2022-03-20 00:55:32 +0800

    add authenticate var

--- versions.tf
+++ versions.tf
@@ -7,6 +7,14 @@ terraform {
   }
 }
 
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
 provider "azurerm" {
-    features {}
+  features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
